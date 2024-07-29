35f3d79e69637edaf4b8c82606646943fbdbdcd9
Igor <igorvlasov732@gmail.com>
2024-07-29 22:42:12 +0400

    LAB0

---
+++ LAB0_YAP/LAB0_YAP.cpp
@@ -0,0 +1,12 @@
+﻿#include <fstream>
+
+int main()
+{
+	std::ifstream inpfile("input.txt");
+	std::ofstream outfile("output.txt");
+	int a, b;
+	inpfile >> a >> b;
+	outfile << a + b;
+	inpfile.close();
+	outfile.close();
+}
